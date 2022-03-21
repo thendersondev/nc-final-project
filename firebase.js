0594e6ac96fd36fefef77044c110c357a40a46a9
Stacey <staceyelsom@gmail.com>
2022-03-21 11:23:25 +0000

    Setup databases on Firebase with docs to submit and recieve data

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { getModularInstance } from '@firebase/util';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyvYh_db-MqSPHwd0pCh7WpkUjFNQa3jI",
+    authDomain: "nc-project-0322.firebaseapp.com",
+    projectId: "nc-project-0322",
+    storageBucket: "nc-project-0322.appspot.com",
+    messagingSenderId: "401909170033",
+    appId: "1:401909170033:web:b552dea99b483fb4c5c0d0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db
