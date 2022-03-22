6b2515c1560223ce3dd3002a055d5a8e3f5c83fc
Stacey <staceyelsom@gmail.com>
2022-03-22 14:29:59 +0000

    Started middleware for handling requests for item information

---
+++ functions/controllers/controller_items.js
@@ -0,0 +1,23 @@
+const {
+    fetchItems,
+    fetchItemsByShop,
+ } = require("../models/model_items")
+
+function getItemsByShop(req,res,next) {
+    fetchItemsByShop(req.params.shop_no).then((data)=>{
+        res.status(200).send({'items': data })
+    })
+    .catch(next)
+}
+
+function getItems(req,res,next) {
+    fetchItems(req.params.shop_no).then((data)=>{
+        res.status(200).send({'items': data })
+    })
+    .catch(next)
+}
+
+module.exports = {
+    getItems,
+    getItemsByShop,
+}
