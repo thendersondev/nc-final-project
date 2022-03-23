ebb368fe0ef037dde3bab81ff32c4ff42b1f0cd7
dawoudcoders <dawoudtanveer@outlook.com>
2022-03-23 21:54:16 +0000

    Completed the styling of the game card to degree

---
+++ frontend/Components/GameCard.js
@@ -0,0 +1,68 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+const GameCard = ({ item }) => {
+  return (
+    <View style={styles.surroundingView}>
+      <View style={styles.cardLeft}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: `https://${item.imgUrl}`,
+          }}
+        ></Image>
+      </View>
+
+      <View style={styles.cardRight}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Shop1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Shop2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Shop3</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  surroundingView: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    margin: 10,
+    borderColor: "#694fad",
+    width: "100%",
+  },
+  image: {
+    width: 110,
+    height: 110,
+    marginLeft:0
+  },
+
+  cardLeft: {
+
+  },
+  cardRight: {
+    flexDirection: "row",
+   
+  },
+  button: {
+    backgroundColor: "#694fad",
+    borderRadius: 10,
+    fontWeight: "700",
+    fontSize: 16,
+    justifyContent: "center",
+    marginHorizontal: 10,
+    height: 50,
+    padding:10,
+  },
+  text: {
+    color: "white",
+  },
+});
+
+export { GameCard };
--- frontend/screens/CompareGamesPage.js
+++ frontend/screens/CompareGamesPage.js
@@ -1,12 +1,57 @@
-import { Text, View, Button } from 'react-native';
-import styles from '../styles/CompareGamesStyles';
-import { StatusBar } from 'expo-status-bar';
+import { Text, View, Button, FlatList } from "react-native";
+import styles from "../styles/CompareGamesStyles";
+import { StatusBar } from "expo-status-bar";
+import { GameCard } from "../Components/GameCard";
 
 export default function CompareGamesPage({ navigation }) {
+  const mockArray = [
+    {
+      title: "Sniper Elite 5 Deluxe Edition - GAME Exclusive",
+      url: "https://www.game.co.uk/en/sniper-elite-5-deluxe-edition-game-exclusive-2879001",
+      price: "£79.99",
+      imgUrl: "img.game.co.uk/ml2/8/1/7/3/817385_gen_b.png",
+      platform: "PlayStation 5",
+    },
+    {
+      title: "Sniper Elite 3 Deluxe Edition - GAME Exclusive",
+      url: "https://www.game.co.uk/en/sniper-elite-5-deluxe-edition-game-exclusive-2879004",
+      price: "£79.99",
+      imgUrl: "img.game.co.uk/ml2/8/1/7/3/817389_gen_b.png",
+      platform: "Xbox Series X",
+    },
+    {
+      title: "Sniper Elite 6 Deluxe Edition - GAME Exclusive",
+      url: "https://www.game.co.uk/en/sniper-elite-5-deluxe-edition-game-exclusive-2878998",
+      price: "£79.99",
+      imgUrl: "img.game.co.uk/ml2/8/1/7/3/817381_ps4_b.png",
+      platform: "PlayStation 4",
+    },
+    {
+      title: "Gran Turismo 7",
+      url: "https://www.game.co.uk/en/gran-turismo-7-2868055",
+      price: "£69.99",
+      imgUrl: "img.game.co.uk/ml2/8/1/1/6/811675_gen_b.png",
+      platform: "PlayStation 5",
+    },
+    {
+      title: "DualSense Wireless Controller - Galactic Purple",
+      url: "https://www.game.co.uk/en/dualsense-wireless-controller-galactic-purple-2871410",
+      price: "£64.99",
+      imgUrl: "img.game.co.uk/ml2/8/1/3/9/813965_gen_b.png",
+      platform: "PlayStation 5",
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <Text>Compare game prices here</Text>
-      {/* <Button title="Go to home" onPress={() => navigation.navigate('Home')} /> */}
+
+      <FlatList
+        data={mockArray}
+        renderItem={GameCard}
+        keyExtractor={(item) => item.title}
+      />
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
