87bf7bea5d2dbacdec3e3414471d57205a14333d
Stacey <staceyelsom@gmail.com>
2022-03-23 14:07:58 +0000

    removed individual scraper upload files and test data

--- backend/webscraper_utils/from_firebase.js
+++
@@ -1,8 +0,0 @@
-import db from "../firebase.js"
-import { collection, getDocs } from "firebase/firestore"; 
-
-const querySnapshot = await getDocs(collection(db, "items/shops/shop2"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-  console.log(doc.data())
-});
