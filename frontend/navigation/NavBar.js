e9f6113f2a29cc8c7abb21c9ef59f1917e253c55
roseymars <roseymarissa@gmail.com>
2022-03-24 14:53:40 +0000

    fix navbar bug

--- frontend/navigation/NavBar.js
+++ frontend/navigation/NavBar.js
@@ -1,12 +1,12 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import HomePage from '../screens/HomePage';
-import LoginPage from '../screens/auth/LoginPage';
-import RegisterPage from '../screens/auth/RegisterPage';
-import CompareGamesPage from '../screens/CompareGamesPage';
-import TradePage from '../screens/TradePage';
-import SearchPage from '../screens/SearchPage';
-import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import HomePage from "../screens/HomePage";
+import LoginPage from "../screens/auth/LoginPage";
+import RegisterPage from "../screens/auth/RegisterPage";
+import CompareGamesPage from "../screens/CompareGamesPage";
+import TradePage from "../screens/TradePage";
+import SearchPage from "../screens/SearchPage";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -17,16 +17,17 @@ export default function NavBar() {
       initialRouteName="Home"
       activeColor="#f0edf6"
       inactiveColor="#3e2465"
-      barStyle={{ backgroundColor: '#694fad' }}
+      barStyle={{ backgroundColor: "#694fad" }}
       tabBarOptions={{
-        barStyle: { position: 'absolute' },
+        barStyle: { position: "absolute" },
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomePage}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: "Home",
+          tabBarColor: "#60489d",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
@@ -37,8 +38,8 @@ export default function NavBar() {
         name="CompareGames"
         component={CompareGamesPage}
         options={{
-          tabBarColor: '#f0edf6',
-          tabBarLabel: 'Compare',
+          tabBarColor: "#694fad",
+          tabBarLabel: "Compare",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons
               name="microsoft-xbox-controller"
@@ -52,7 +53,8 @@ export default function NavBar() {
         name="TradePage"
         component={TradePage}
         options={{
-          tabBarLabel: 'Trade',
+          tabBarColor: "#7357ba",
+          tabBarLabel: "Trade",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons
               name="swap-vertical-variant"
@@ -66,8 +68,8 @@ export default function NavBar() {
         name="SearchPage"
         component={SearchPage}
         options={{
-          tabBarColor: '#f0edf6',
-          tabBarLabel: 'Search',
+          tabBarColor: "#775ac2",
+          tabBarLabel: "Search",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons
               name="card-search"
