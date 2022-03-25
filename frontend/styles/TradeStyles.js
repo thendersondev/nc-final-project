0ba9ea3fe8bb11427b28c0b4c09da41ba4539fef
JCouz <couzensj@live.co.uk>
2022-03-25 10:45:07 +0000

    add ability to now display trade cards

--- frontend/styles/TradeStyles.js
+++ frontend/styles/TradeStyles.js
@@ -12,6 +12,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#694FAD',
   },
+  inputTitle: {
+    fontWeight: '700',
+    fontSize: 20,
+    paddingLeft: 4,
+    color: '#694FAD',
+    borderRadius: 5,
+    borderWidth: 2,
+  },
 });
 
 export default styles;
