dc652f4d8c311cc2d0d75471350c4dffc56d3ad5
Stacey <staceyelsom@gmail.com>
2022-03-29 00:53:08 +0100

    Added screen for user reviews

--- frontend/navigation/TradeStackNav.js
+++ frontend/navigation/TradeStackNav.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MessagePage from "../Components/MessagePage";
 import PostTrade from "../Components/PostTrade";
+import PostReview from "../Components/PostReview";
 import UserPage from "../Components/UserPage";
 import TradePage from "../screens/TradePage";
 
@@ -17,6 +18,11 @@ export default function TradeStackNavigator() {
       <Stack.Screen
         name="Post"
         component={PostTrade}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Review"
+        component={PostReview}
         options={{ headerShown: false }}
       />
       <Stack.Screen
