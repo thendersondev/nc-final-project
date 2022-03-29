9b4b8c127b824291388dce2b0ea30d9daf5a24c4
thendo90 <thenderson@protonmail.com>
2022-03-29 10:53:21 +0100

    merge messaging into main

--- frontend/Components/TradeGameCard.js
+++ frontend/Components/TradeGameCard.js
@@ -1,5 +1,5 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import styles from '../styles/TradeGameCardStyles';
+import styles from "../styles/TradeGameCardStyles";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 const TradeGameCard = ({ item }, navigation) => {
   const { username } = item;
@@ -8,38 +8,33 @@ const TradeGameCard = ({ item }, navigation) => {
       <View style={styles.cardLeft}>
         <Image
           style={styles.image}
-          source={require('../assets/placeholder.png')}
+          source={require("../assets/placeholder.png")}
         ></Image>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.navigate("Message", { username });
+          }}
+        >
+          <Text style={styles.buttonText}>Message</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.navigate("Profile", { username });
+          }}
+        >
+          <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.cardRight}>
-        <View style={styles.cardRightTop}>
-          <Text style={styles.gameTitle}>
-            {item.title} - ({item.platform})
-          </Text>
+        <Text style={styles.gameTitle}>{item.title}</Text>
+        <Text style={styles.gameDetails}>{item.platform}</Text>
+        <Text style={styles.gameDetails}>Price: {item.price}</Text>
+        <Text style={styles.gameDetails}>Location: {item.location}</Text>
+        <View style={styles.cardRightBottom}>
           <Text style={styles.gameDetails}>User: {item.username}</Text>
-          <Text style={styles.gameDetails}>Price: {item.price}</Text>
-          <Text style={styles.gameDetails}>Condition: {item.condition}</Text>
-          <Text style={styles.gameDetails}>Location: {item.location}</Text>
-        </View>
-
-        <View style={styles.cardLeft}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              navigation.navigate('Message', { username });
-            }}
-          >
-            <Text style={styles.text}>Message</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              navigation.navigate('Profile', { username });
-            }}
-          >
-            <Text style={styles.text}>View Profile</Text>
-          </TouchableOpacity>
         </View>
       </View>
     </View>
