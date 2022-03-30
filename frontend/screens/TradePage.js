52d26bde3bc9600b8cab4230de492078e30e212a
Stacey <staceyelsom@gmail.com>
2022-03-30 12:42:24 +0100

    resolving merge conflict

--- frontend/screens/TradePage.js
+++ frontend/screens/TradePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import styles from '../styles/TradeStyles';
 import { StatusBar } from 'expo-status-bar';
@@ -8,22 +7,11 @@ import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { fetchTrades } from '../models/model_trades';
 import { auth } from '../firebase';
-=======
-import { Text, View, FlatList, TouchableOpacity } from "react-native";
-import styles from "../styles/TradeStyles";
-import { StatusBar } from "expo-status-bar";
-import { TradeGameCard } from "../Components/TradeGameCard";
-import { v4 as uuidv4 } from "uuid";
-import React, { useEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { fetchTrades } from "../models/model_trades";
->>>>>>> f3738dddce5a9ea1f52d47fe792ac71b1eedcbf0
 
 export default function TradePage() {
   const navigation = useNavigation();
   const [trades, setTrades] = useState([]);
   const [tradeables, setTradebles] = useState();
-<<<<<<< HEAD
   const [query, setQuery] = useState('Xbox SeriesX PS5 Nintendo Switch');
   const [refresh , setRefresh] = useState(0) 
 
@@ -40,23 +28,6 @@ export default function TradePage() {
       setTrades(array)
     })
   }, [ refresh ]);
-=======
-  const [query, setQuery] = useState("Xbox SeriesX PS5 Nintendo Switch");
-
-  useEffect(() => {
-    fetchTrades().then((data) => {
-      const keys = Object.keys(data);
-      const values = Object.values(data);
-      console.log(values);
-      const array = [];
-      keys.forEach((key) => {
-        data[key].key = key;
-        array.push(data[key]);
-      });
-      setTrades(Object.values(data));
-    });
-  }, [TradeGameCard]);
->>>>>>> f3738dddce5a9ea1f52d47fe792ac71b1eedcbf0
 
   return (
     <View style={styles.container}>
