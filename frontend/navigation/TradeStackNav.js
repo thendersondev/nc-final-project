fff35f9072a61728733d1938604b193fc2a56347
thendo90 <thenderson@protonmail.com>
2022-03-30 15:10:47 +0100

    fix little issues

--- frontend/navigation/TradeStackNav.js
+++ frontend/navigation/TradeStackNav.js
@@ -1,4 +1,3 @@
-
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MessagePage from "../Components/MessagePage";
 import PostTrade from "../Components/PostTrade";
@@ -6,7 +5,6 @@ import PostReview from "../Components/PostReview";
 import UserPage from "../Components/UserPage";
 import TradePage from "../screens/TradePage";
 import ChatsPage from "../screens/ChatsPage";
-
 
 const Stack = createNativeStackNavigator();
 
@@ -16,77 +14,72 @@ export default function TradeStackNavigator() {
       <Stack.Screen
         name="Trade"
         component={TradePage}
-
         options={{
-          headerTintColor: '694fad',
+          headerTintColor: "694fad",
           headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#694fad',
-            headerShown: false
+            fontWeight: "bold",
+            color: "#694fad",
+            headerShown: false,
           },
+          headerShown: false,
         }}
-
       />
       <Stack.Screen
         name="Post"
         component={PostTrade}
-
-         options={{
-          headerTintColor: '694fad',
+        options={{
+          headerTintColor: "694fad",
           headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#694fad',
-            headerShown: false
+            fontWeight: "bold",
+            color: "#694fad",
+            headerShown: false,
           },
+          headerShown: false,
         }}
-
       />
       <Stack.Screen
         name="Review"
         component={PostReview}
-
-       options={{
-          headerTintColor: '694fad',
+        options={{
+          headerTintColor: "694fad",
           headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#694fad',
-            headerShown: false
+            fontWeight: "bold",
+            color: "#694fad",
+            headerShown: false,
           },
+          headerShown: false,
         }}
-
       />
       <Stack.Screen
         name="Message"
         component={MessagePage}
-
-       options={{
-          headerTintColor: '694fad',
+        options={{
+          headerTintColor: "694fad",
           headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#694fad',
-            headerShown: false
+            fontWeight: "bold",
+            color: "#694fad",
+            headerShown: false,
           },
+          headerShown: false,
         }}
-
       />
       <Stack.Screen
         name="Profile"
         component={UserPage}
-
         options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Chats"
         component={ChatsPage}
         options={{
-          headerTintColor: '694fad',
+          headerTintColor: "694fad",
           headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#694fad',
-            headerShown: false
+            fontWeight: "bold",
+            color: "#694fad",
+            headerShown: false,
           },
+          headerShown: false,
         }}
-
       />
     </Stack.Navigator>
   );
